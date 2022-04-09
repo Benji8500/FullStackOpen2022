395b613366b024ac44de03e833882f078cdf5e4d
Benjamin-Soto <benji.8500@gmail.com>
2022-04-09 18:48:03 -0400

    Anecdotes created

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type buttonProps ={
+    handleClick:React.MouseEventHandler,
+    text:string
+}
+const Button = ({handleClick, text}:buttonProps)=>
+    <button onClick={handleClick}>
+        {text}
+    </button>
+
+type resultProps ={
+    value:number,
+    text:string
 }
 
-export default App;
+const StatisticsLine = ({value,text}:resultProps)=> <><td>{text} </td><td>{value} {text ==="positive" ? "%" : ""}</td></>
+
+
+type statisticsProps ={
+    good:number,
+    neutral:number,
+    bad:number
+}
+const Statistics =({good,neutral,bad}:statisticsProps) =>{
+    if (good===0 && bad===0 && neutral==0){
+        return (
+            <p>No feedback given.</p>
+        );
+    }
+    return (
+        <div>
+            <h1>statistics</h1>
+            <table>
+                <tr><StatisticsLine value={good} text={"good"}/></tr>
+                <tr><StatisticsLine value={neutral} text={"neutral"}/></tr>
+                <tr><StatisticsLine value={bad} text={"bad"}/></tr>
+                <tr><StatisticsLine value={bad+good+neutral} text={"all"}/></tr>
+                <tr><StatisticsLine value={(good+bad*-1)/(good+bad+neutral)} text={"average"}/></tr>
+                <tr><StatisticsLine value={(good*100)/(good+bad+neutral)} text={"positive"}/></tr>
+            </table>
+
+        </div>
+    )
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+      <div>
+          <h1>give feedback</h1>
+          <Button handleClick={()=>{setGood(good+1)}} text={"good"}/>
+          <Button handleClick={()=>{setNeutral(neutral+1)}} text={"neutral"}/>
+          <Button handleClick={()=>{setBad(bad+1)}} text={"bad"}/>
+          <Statistics good={good} neutral={neutral} bad={bad}/>
+      </div>
+  )
+}
+
+export default App
